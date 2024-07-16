937a608ceaf4b2476f5ddce3bed9dc7cfa85ebf7
blueDeath3301 <125863207+blueDeath3301@users.noreply.github.com>
2024-07-16 02:37:46 -0700

    Working code uploaded

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # capturePhoto-rs
 
-Trying to capture a photo using the Media Capture API. Code on main.rs does not work and throws the error
+Trying to capture a photo using the Media Capture API. Code works and saves the captured image in JPEG.
 
-Os {code: -2147483634, kind: Uncategorized, message: "A method was called at an unexpected time." }
 
- so I'm trying to use
-the MediaCapture API instead, which is on the main2.rs file
 
 CREDITS: @Rafael (Discord)
--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,55 @@
-
-/*
-We are going to capture photos from the camera and save them to a file as explained on the README.md file
-using Windows-rs crate to interact with the Windows API.
- */
-
-
-pub mod main2;
-
+use std::{env, fs::OpenOptions};
 use windows::{
-    Foundation::{IAsyncOperation, Size},
-    Media::Capture::{CameraCaptureUI, CameraCaptureUIPhotoFormat, CameraCaptureUIMode},
+    core::HSTRING,
+    Media::{
+        Capture::{MediaCapture, MediaCaptureInitializationSettings, PhotoCaptureSource},
+        MediaProperties::ImageEncodingProperties,
+    },
     Storage::StorageFile,
 };
 
-use windows::core::HRESULT;
-use windows::Win32::System::Com::{CoInitializeEx, CoUninitialize};
-use windows::Win32::System::Com::COINIT_APARTMENTTHREADED;
+fn main() -> windows::core::Result<()> {
+    //
+    // Create new MediaCapture instance and initialize it.
+    // https://learn.microsoft.com/uwp/api/windows.media.capture.photocapturesource
+    //
 
+    let media_capture = MediaCapture::new()?;
 
+    let settings = MediaCaptureInitializationSettings::new()?;
+    settings.SetPhotoCaptureSource(PhotoCaptureSource::VideoPreview)?;
 
-fn capture_photo() -> Result<(), std::io::Error> {
-    let capture_ui = CameraCaptureUI::new()?;
-    capture_ui.PhotoSettings()?.SetFormat(CameraCaptureUIPhotoFormat(0))?; //JPEG
-    capture_ui.PhotoSettings()?.SetCroppedSizeInPixels(Size{Width: 200.0, Height: 200.0})?;
+    media_capture
+        .InitializeWithSettingsAsync(&settings)?
+        .get()?;
 
-    let photo: IAsyncOperation<StorageFile> = capture_ui.CaptureFileAsync(CameraCaptureUIMode(1))?;
+    //
+    // Prepare file for output. MediaCapture requires a StorageFile
+    // pointing to an existing file with a non-extended absolute path.
+    // https://learn.microsoft.com/uwp/api/windows.media.mediaproperties.imageencodingproperties.createjpeg
+    //
 
-    // Wait for the photo to be captured
-    //let _photo = photo.Completed();
+    let encoding = ImageEncodingProperties::CreateJpeg()?;
 
-    //we must handle the any error that may occur for the AsynchOperationCompletionHandler
-    match photo.Status() {
-        Ok(_) => {
-            let photo = photo.GetResults()?;
-            println!("Photo captured: {:?}", photo.Path()?);
-        }
-        Err(e) => {
-            println!("Error capturing photo: {:?}", e);
-        }
-    }
+    let path = env::current_dir().unwrap().join("Output.jpg");
+    let _ = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .create(true)
+        .open(&path)
+        .unwrap()
+        .set_len(0);
+
+    let file = StorageFile::GetFileFromPathAsync(&HSTRING::from(path.as_os_str()))?.get()?;
+
+    //
+    // Capture photo from video source
+    // https://learn.microsoft.com/uwp/api/windows.media.capture.mediacapture.capturephototostoragefileasync
+    //
+
+    media_capture
+        .CapturePhotoToStorageFileAsync(&encoding, &file)?
+        .get()?;
 
     Ok(())
 }
-
-
-fn main() {
-
-    //initialize COM in STA mode
-    unsafe {
-       let init_com = CoInitializeEx(None, COINIT_APARTMENTTHREADED);
-
-         if init_com != HRESULT(0) {
-              println!("Error initializing COM for this thread: {:?}", init_com);
-              return 
-         }
-    
-    }   
-    let capture = capture_photo();
-    match capture {
-        Ok(_) => println!("Photo captured successfully"),
-        Err(e) => println!("Error capturing photo: {:?}", e),
-    }
-    //uninitialize COM
-    unsafe {
-        CoUninitialize();
-    }
-
-}
- 
- 
-
-
-
